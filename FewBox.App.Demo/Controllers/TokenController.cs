f0cfeef7359f15bb6fbe4eca0d466aa22d74ff50
Peter Pang <mv@live.cn>
2019-01-23 18:53:23 +0800

    Add authentication web api.

---
+++ FewBox.App.Demo/Controllers/TokenController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FewBox.App.Demo.Dtos;
+using FewBox.Core.Persistence.Cache;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using FewBox.Core.Persistence.Orm;
+using System.Security.Claims;
+using FewBox.App.Demo.Configs;
+
+namespace FewBox.App.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private ITokenService TokenService { get; set; }
+        private JWTConfig JWTConfig { get; set; }
+
+        public TokenController(ITokenService tokenService, JWTConfig jWTConfig)
+        {
+            this.TokenService = tokenService;
+            this.JWTConfig = jWTConfig;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public SignInResponseDto CreateToken([FromBody]SignInRequestDto signInRequestDto)
+        {
+            if(signInRequestDto.Username=="landpy" && signInRequestDto.Password=="fewbox")
+            {
+                var currentUser = new CurrentUser { 
+                    Id = Guid.NewGuid().ToString(),
+                    Key = this.JWTConfig.Key,
+                    Issuer = this.JWTConfig.Issuer,
+                    Claims = new List<Claim>{  }
+                };
+                string token = this.TokenService.GenerateToken(currentUser, TimeSpan.FromMinutes(5));
+                return new SignInResponseDto { IsValid = true, Token = token };
+            }
+            else
+            {
+                return new SignInResponseDto { IsValid = false };
+            }
+        }
+
+    }
+}
