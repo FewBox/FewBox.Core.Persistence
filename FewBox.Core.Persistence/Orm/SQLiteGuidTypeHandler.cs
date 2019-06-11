57d8118f7cfd1630411eaa5a60fe1ccfbde1ce12
fewbox <fewbox.com@gmail.com>
2019-06-11 14:23:04 +0800

    Rename. [ci skip]

---
+++ FewBox.Core.Persistence/Orm/SQLiteGuidTypeHandler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+
+namespace FewBox.Core.Persistence.Orm
+{
+    public class SQLiteGuidTypeHandler : Dapper.SqlMapper.TypeHandler<Guid>
+    {
+        public override Guid Parse(object value)
+        {
+            Guid guid = Guid.Empty;
+            if(value is byte[])
+            {
+                guid = new Guid((byte[])value);
+            }
+            return guid;
+        }
+
+        public override void SetValue(IDbDataParameter parameter, Guid value)
+        {
+            parameter.Value = value.ToString();
+        }
+    }
+}
